82af3b47b243188e3ffe63a31ea50b4b6aaebe13
慕云 <1219258598@qq.com>
2022-06-02 22:38:46 +0800

    补充提交run.py

--- 考勤小程序/交付版本v2/run.py
+++ 考勤小程序/交付版本v2/run.py
@@ -1,4 +1,26 @@
-import 考勤服务 as server
+import os.path
+import sys
+
+sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('./考勤程序'))
+sys.path.append(os.path.abspath('./考勤程序/utils'))
+from 考勤程序 import server, service
+from 考勤程序.utils import setting_utils, file_utils
+
+settings = setting_utils.read_settings()
+TO_FILE = "考勤总表.xlsx"
+
+
+def init():
+    """
+    初始化：
+    复制文件
+    """
+    source_file = service.get_read_file()
+    to_file = settings['save_folder'] + "/" + TO_FILE
+    file_utils.copy_file(source_file, to_file)
+
 
 if __name__ == '__main__':
+    init()
     server.app.run(debug=True)
